f34b5b8b1258d2d779db352266c1ba298cf2fa57
manuel <manuel.hernandez28@outlook.com>
2025-06-22 21:49:42 -0600

    Actualizar index.html y saludo.js: corregir formato de meta y agregar comentarios explicativos en saludo.js

--- js/saludo.js
+++ js/saludo.js
@@ -5,14 +5,32 @@ alert("hola mundo con JavaScript"); // * Mensaje de saludo
 
 //& variables tipo VAR
  var nombre = "Manuel"; // * Declaración de variable string
- var edad; 
+ var edad; // * Declaración de variable undefined
  var presenteONo; // * Declaración de variable booleana
  var apellido;
+ var estatura; // * Declaración de variable float
 
  alert("HOLA " + nombre); // * Mostrar el valor de la variable nombre
 
  // ^ PARTE 3: ASIGNACION, CONSOLE LOG Y TYPEOF EN JAVASCRIPT
 
- 
+//& Asignación de valores a las variables
+    nombre = "Manuel"; // * Asignación de valor a la variable nombre
+    edad = 24; // * Asignación de valor a la variable edad
+    presenteONo = true; // * Asignación de valor a la variable booleana presenteONo
+    apellido = "Hernández"; // * Asignación de valor a la variable apellido
+    estatura = 1.75; // * Asignación de valor a la variable estatura
+
+//& Uso de console.log para mostrar valores y typeof para verificar tipos de datos
+
+    console.log("Nombre: " + nombre + " " + typeof nombre); // * Mostrar el valor de la variable nombre en la consola y su tipo
+    console.log("Edad: " + edad + " " + typeof edad); // * Mostrar el valor de la variable edad en la consola y su tipo
+    console.log("Presente o No: " + presenteONo + " " + typeof presenteONo); // * Mostrar el valor de la variable booleana presenteONo en la consola y su tipo
+    console.log("Apellido: " + apellido + " " + typeof apellido); // * Mostrar el valor de la variable apellido en la consola y su tipo
+    console.log("Estatura: " + estatura + " " + typeof estatura); // * Mostrar el valor de la variable estatura en la consola y su tipo
+
+
+
+
 
 
