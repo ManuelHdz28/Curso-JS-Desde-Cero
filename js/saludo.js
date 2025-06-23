61cc54e0ac7e8ea75f1bdce44b61e88d8d4ff2c5
manuel <manuel.hernandez28@outlook.com>
2025-06-22 22:16:13 -0600

    Actualizar saludo.js: agregar explicaciones sobre variables tipo var, let y const, y mejorar comentarios en el código

--- js/saludo.js
+++ js/saludo.js
@@ -1,7 +1,7 @@
-// ^ PARTE 1: INCIALIZA TU PRIMER PROGRAMA 
+// ^ PARTE 1: INCIALIZA TU PRIMER PROGRAMA EN JAVASCRIPT
 alert("hola mundo con JavaScript"); // * Mensaje de saludo
 
-// ^ PARTE 2: VARIABLES Y TIPOS DE DATOS
+// ^ PARTE 2: VARIABLES Y TIPOS DE DATOS EN JAVASCRIPT
 
 //& variables tipo VAR
  var nombre = "Manuel"; // * Declaración de variable string
@@ -28,6 +28,48 @@ alert("hola mundo con JavaScript"); // * Mensaje de saludo
     console.log("Presente o No: " + presenteONo + " " + typeof presenteONo); // * Mostrar el valor de la variable booleana presenteONo en la consola y su tipo
     console.log("Apellido: " + apellido + " " + typeof apellido); // * Mostrar el valor de la variable apellido en la consola y su tipo
     console.log("Estatura: " + estatura + " " + typeof estatura); // * Mostrar el valor de la variable estatura en la consola y su tipo
+    console.log("---------------------------------------------------------------------------------------------"); // * Línea divisoria en la consola
+
+// ^ PARTE 4: DIFERENCIA ENTRE VAR, LET Y CONST EN JAVASCRIPT
+
+//& Variables tipo var
+/* 
+    ! las variables tippo var son variables que pueden ser redeclaradas y actualizadas en cualquier parte del código,
+    ! lo que puede llevar a errores si no se tiene cuidado. Se recomienda usar let o const para evitar problemas de alcance 
+    ! y redeclaración.
+*/
+
+if (true) {
+    var variableTipoVar = "Soy una variable tipo var"; // * Variable tipo var
+}
+
+console.log(variableTipoVar); // * Mostrar el valor de la variable tipo var en la consola
+
+
+//& Variables tipo let
+/* 
+    ! las variables tipo let son variables que pueden ser actualizadas pero no redeclaradas en el mismo alcance,
+    ! lo que las hace más seguras que las variables tipo var. Se recomienda usar let para variables que necesitan ser actualizadas.
+*/
+if (true) {
+    let variableTipoLet = "Soy una variable tipo let"; // * Variable tipo let
+    // * Las variables tipo let tienen un alcance de bloque, lo que significa que solo son accesibles dentro del bloque donde se declaran.
+    // * Esto ayuda a evitar problemas de alcance y redeclaración.
+    console.log(variableTipoLet); // * Mostrar el valor de la variable tipo let en la consola
+}
+
+//& Variables tipo const
+/* 
+    ! las variables tipo const son variables que no pueden ser actualizadas ni redeclaradas, lo que las hace ideales para valores constantes.
+    ! Se recomienda usar const para valores que no cambian a lo largo del tiempo.
+*/
+
+const variableTipoConst = "Soy una variable tipo const"; // * Variable tipo const
+
+// variableTipoConst = "Intentando actualizar una variable const"; // * Esto generará un error porque las variables tipo const no pueden ser actualizadas
+// * Las variables tipo const deben ser inicializadas en el momento de su declaración y no pueden ser actualizadas o redeclaradas.
+console.log(variableTipoConst); // * Mostrar el valor de la variable tipo const en la consola
+
 
 
 
