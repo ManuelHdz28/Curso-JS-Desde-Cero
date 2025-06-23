0fc25d953394a66e5fdeea3bf79efb19dc9650b1
manuel <manuel.hernandez28@outlook.com>
2025-06-22 21:28:17 -0600

    Actualizar index.html y saludo.js: cambiar idioma a español y agregar comentarios explicativos en saludo.js

--- js/saludo.js
+++ js/saludo.js
@@ -1,1 +1,18 @@
-alert("hola mundo con JavaScript");
+// ^ PARTE 1: INCIALIZA TU PRIMER PROGRAMA 
+alert("hola mundo con JavaScript"); // * Mensaje de saludo
+
+// ^ PARTE 2: VARIABLES Y TIPOS DE DATOS
+
+//& variables tipo VAR
+ var nombre = "Manuel"; // * Declaración de variable string
+ var edad; 
+ var presenteONo; // * Declaración de variable booleana
+ var apellido;
+
+ alert("HOLA " + nombre); // * Mostrar el valor de la variable nombre
+
+ // ^ PARTE 3: ASIGNACION, CONSOLE LOG Y TYPEOF EN JAVASCRIPT
+
+ 
+
+
